fe1f61be934e5d3b6d54ed088f1a4ebb3a23a0cd
qpwo0258 <77845353+qpwo0258@users.noreply.github.com>
2022-07-12 00:54:30 +0900

    적, 휴지통(미완성)

---
+++ Assets/02.Scripts/Enemy/MonsterSpawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    [SerializeField] private Monster monsterPrefab;
+    [SerializeField] private Transform tower;
+    [SerializeField] private Transform wallPaper;
+    [SerializeField] private float spawnRange = 15f;
+    [SerializeField] private float spawnDelay = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PoolManager.Instance.CreatePool(monsterPrefab, 50);
+        StartCoroutine(SpawnMonster());
+    }
+
+    private IEnumerator SpawnMonster()
+    {
+        while (true)
+        {
+            Monster obj = PoolManager.Instance.Pop(monsterPrefab) as Monster;
+            obj.transform.position = GetRandomCirclePoint();
+            obj.SetTarget(tower);
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+
+    private Vector3 GetRandomCirclePoint()
+    {
+        Vector2 point = Random.insideUnitCircle.normalized * spawnRange;
+        return new Vector3(point.x, wallPaper.position.y + 1, point.y);
+    }
+}
---
+++ Assets/02.Scripts/RecycleBin/OnChangePosition.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnChangePosition : MonoBehaviour
+{
+    public PolygonCollider2D hole2DCollider;
+    public PolygonCollider2D ground2DCollider;
+    public MeshCollider GeneratedMeshCollider;
+    public float initialScale;
+    Mesh GeneratedMesh;
+
+    private void FixedUpdate()
+    {
+        if (transform.hasChanged == true)
+        {
+            transform.hasChanged = false;
+            hole2DCollider.transform.position = new Vector2(transform.position.x, transform.position.z);
+            hole2DCollider.transform.localScale = transform.localScale * initialScale;
+            MakeHole2D();
+            Make3DMeshCollider();
+        }
+    }
+
+    private void MakeHole2D()
+    {
+        Vector2[] PointPositions = hole2DCollider.GetPath(0);
+        for(int i = 0; i < PointPositions.Length; i++)
+        {
+            PointPositions[i] = hole2DCollider.transform.TransformPoint(PointPositions[i]);
+        }
+        ground2DCollider.pathCount = 2;
+        ground2DCollider.SetPath(1, PointPositions);
+    }
+
+    private void Make3DMeshCollider()
+    {
+        if (GeneratedMesh != null) Destroy(GeneratedMesh);
+        GeneratedMesh = ground2DCollider.CreateMesh(true, true);
+        GeneratedMeshCollider.sharedMesh = GeneratedMesh;
+    }
+}
