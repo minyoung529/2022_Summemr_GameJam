d248862276427d5bb265ba40fccc70f9c620605a
minyoung529 <dlalsdud102613@gmail.com>
2022-07-12 12:54:27 +0900

    메일 시스템 합침

--- Assets/02.Scripts/MyPc/FileTower.cs
+++ Assets/02.Scripts/MyPc/FileTower.cs
@@ -24,26 +24,28 @@ public class FileTower : MonoBehaviour
 
     void Update()
     {
-        file.transform.position =_cam.WorldToScreenPoint(transform.position + new Vector3(0, -0.8f, 0));
+        file.transform.position = _cam.WorldToScreenPoint(transform.position + new Vector3(0, -0.8f, 0));
 
-        if(fileType == 1)
-        fileImage.fillAmount = Mathf.Lerp(fileImage.fillAmount, GameManager.Instance.dadTowerGage / 100, Time.deltaTime * fullSpeed);
-        if(fileType == 2)
-        fileImage.fillAmount = Mathf.Lerp(fileImage.fillAmount, GameManager.Instance.brotherTowerGage / 100, Time.deltaTime * fullSpeed);
+        if (fileType == 1)
+            fileImage.fillAmount = Mathf.Lerp(fileImage.fillAmount, GameManager.Instance.dadTowerGage / 100, Time.deltaTime * fullSpeed);
+        if (fileType == 2)
+            fileImage.fillAmount = Mathf.Lerp(fileImage.fillAmount, GameManager.Instance.brotherTowerGage / 100, Time.deltaTime * fullSpeed);
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.collider.CompareTag("Monster"))
+        if (collision.collider.CompareTag("Monster"))
         {
             //공격 받음 type에 따라 공격받은 거점 용량 증가
-            if(fileType==1)
+            if (fileType == 1)
             {
                 GameManager.Instance.dadTowerGage += 10;
             }
-            if(fileType==2)
+            if (fileType == 2)
             {
                 GameManager.Instance.brotherTowerGage += 10;
             }
+
+            collision.transform.GetComponent<Monster>().Die();
         }
     }
 }
