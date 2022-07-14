5b194a40e2094a6b5a01e4f1a89adad998e391f9
minyoung529 <dlalsdud102613@gmail.com>
2022-07-14 19:55:45 +0900

    패턴 코루틴

--- Assets/02.Scripts/Enemy/MonsterSpawner.cs
+++ Assets/02.Scripts/Enemy/MonsterSpawner.cs
@@ -17,6 +17,7 @@ public class MonsterSpawner : MonoBehaviour
     private float time = 0f;
 
     private bool isSquareCoroutine = false;
+    private bool isHorVer = false;
 
     void Start()
     {
@@ -55,31 +56,33 @@ public class MonsterSpawner : MonoBehaviour
     {
         Monster centerMonster = PoolManager.Instance.Pop(monsterPrefab[(int)type]) as Monster;
         GameManager.Instance.monsters.Add(centerMonster);
-        centerMonster.transform.position = GetRandomCirclePoint();
+        centerMonster.transform.position = GetLeftRightPoint();
         centerMonster.spawner = this;
+
         int towerT = Random.Range(0, tower.Count);
         centerMonster.SetTarget(tower[towerT].transform);
 
-        for (int i=1;i<=cnt;i++)
+        for (int i = 1; i <= cnt; i++)
         {
             Monster obj = PoolManager.Instance.Pop(monsterPrefab[(int)type]) as Monster;
             Monster obj2 = PoolManager.Instance.Pop(monsterPrefab[(int)type]) as Monster;
             GameManager.Instance.monsters.Add(obj);
             GameManager.Instance.monsters.Add(obj2);
-            obj.transform.position = centerMonster.transform.position+new Vector3(0, 0, dis*i);
-            obj2.transform.position = centerMonster.transform.position+new Vector3(0, 0, (-dis)*i);
+            obj.transform.position = centerMonster.transform.position + new Vector3(0, 0, dis * i);
+            obj2.transform.position = centerMonster.transform.position + new Vector3(0, 0, (-dis) * i);
             obj.spawner = this;
             obj2.spawner = this;
-            obj.SetTargetRigid(centerMonster.rigid);
-            obj2.SetTargetRigid(centerMonster.rigid);
+            obj.SetTargetRigid(centerMonster);
+            obj2.SetTargetRigid(centerMonster);
         }
     }
     void SpawnHorArr(MonsterType type, int cnt, float dis)
     {
         Monster centerMonster = PoolManager.Instance.Pop(monsterPrefab[(int)type]) as Monster;
         GameManager.Instance.monsters.Add(centerMonster);
-        centerMonster.transform.position = GetRandomCirclePoint();
+        centerMonster.transform.position = GetTopBottomPoint();
         centerMonster.spawner = this;
+
         int towerT = Random.Range(0, tower.Count);
         centerMonster.SetTarget(tower[towerT].transform);
 
@@ -93,20 +96,16 @@ public class MonsterSpawner : MonoBehaviour
             obj2.transform.position = centerMonster.transform.position + new Vector3((-dis) * i, 0, 0);
             obj.spawner = this;
             obj2.spawner = this;
-            obj.SetTargetRigid(centerMonster.rigid);
-            obj2.SetTargetRigid(centerMonster.rigid);
+            obj.SetTargetRigid(centerMonster);
+            obj2.SetTargetRigid(centerMonster);
         }
     }
 
     private int GetMonsterType()
     {
-        if (time <= 50f)
+        if (time <= 70f)
         {
             return (int)MonsterType.BASIC;
-        }
-        else
-        {
-            spawnDelay = 0.1f;
         }
 
         int rand = Random.Range(1, 101);
@@ -125,6 +124,20 @@ public class MonsterSpawner : MonoBehaviour
     {
         Vector2 point = Random.Range(0, 2) == 1 ? new Vector2(spawnRange.x, Random.Range(-spawnRange.y, spawnRange.y)) : new Vector2(Random.Range(-spawnRange.x, spawnRange.x), spawnRange.y);
         point *= Random.Range(0, 2) == 1 ? 1 : -1;
+        return new Vector3(point.x, wallPaper.position.y + 1, point.y);
+    }
+
+    private Vector3 GetTopBottomPoint()
+    {
+        Vector2 point = new Vector2(Random.Range(-spawnRange.x, spawnRange.x), spawnRange.y);
+        point.y *= Random.Range(0, 2) == 1 ? 1 : -1;
+        return new Vector3(point.x, wallPaper.position.y + 1, point.y);
+    }
+
+    private Vector3 GetLeftRightPoint()
+    {
+        Vector2 point = new Vector2(spawnRange.x, Random.Range(-spawnRange.y, spawnRange.y));
+        point.x *= Random.Range(0, 2) == 1 ? 1 : -1;
         return new Vector3(point.x, wallPaper.position.y + 1, point.y);
     }
 
@@ -173,10 +186,23 @@ public class MonsterSpawner : MonoBehaviour
 
     internal void AddPoint(int score)
     {
-        if (score > 60 && !isSquareCoroutine)
+        if (score > 3500 && !isSquareCoroutine)
         {
             isSquareCoroutine = true;
             StartCoroutine(SquareCoroutine());
+        }
+        else if(score > 2000 && !isHorVer)
+        {
+            isHorVer = true;
+            StartCoroutine(VerHorCoroutine());
+        }
+        else if(score>300)
+        {
+            spawnDelay = 0.2f;
+        }
+        else if(score > 1000)
+        {
+            spawnDelay = 0.1f;
         }
     }
 
@@ -184,9 +210,30 @@ public class MonsterSpawner : MonoBehaviour
     {
         while (true)
         {
+            if (tower.Count == 0) yield break;
+
             CirclePattern(Random.Range(3f, 5f), MonsterType.FAST, 60);
             Debug.Log("Ciircle");
-            yield return new WaitForSeconds(15f);
+            yield return new WaitForSeconds(25f);
+        }
+    }
+
+    private IEnumerator VerHorCoroutine()
+    {
+        while (true)
+        {
+            if (tower.Count == 0) yield break;
+
+            if (Random.Range(0, 2) == 0)
+            {
+                SpawnHorArr((MonsterType)Random.Range(1, 3), 3, 2);
+            }
+            else
+            {
+                SpawnVerArr((MonsterType)Random.Range(1, 3), 3, 2);
+            }
+
+            yield return new WaitForSeconds(25f);
         }
     }
 }
